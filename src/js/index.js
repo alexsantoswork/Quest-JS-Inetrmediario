89f6b62449192b88541be521e77ac9b2bd9ff3b8
Alex Santos <1984alexsantos@gmail.com>
2024-08-18 13:01:51 -0300

    finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,70 @@
-const inputText = document.querySelectorAll(".input-text, .mensagem");
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordtwo = document.getElementById('password-two')
 
-inputText.forEach(input => {
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-input.addEventListener('change', () => {
+    checkInputs()
+})
 
-if (input.value !== "") {
+function checkInputs() {
 
-input.classList.add("preenchido")
+    const usernameValue = username.value.trim()
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+    const passwordtwoValue = passwordtwo.value.trim()
 
-} else {
+    if(usernameValue === '') {
+        setErrorFor(username, 'campo obrigatório')
+    } else {
+        setSuccessFor(username)
+    }
 
-input.classList.remove("preenchido")
+    if(emailValue === '') {
+        setErrorFor(email, 'campo obrigatório')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email inválido')
+    } else {
+        setSuccessFor(email)
+    }
+   
+    if(passwordValue === '') {
+        setErrorFor(password, 'campo obrigatório')
 
+    } else if(passwordValue.length < 1) { 
+        setErrorFor(password, '')
+    } else {
+        setSuccessFor(password)
+    }
+
+    if(passwordtwoValue === '') {
+        setErrorFor(passwordtwo, 'campo obrigatório')
+
+    } else if(passwordValue == passwordtwoValue) { 
+        setErrorFor(passwordtwo, '')
+    } else {
+        setSuccessFor(passwordtwo)
+    }
 }
-});
-});
+
+function setErrorFor(input, message,) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    small.innerText = message
+
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input,) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
+}
+
+function isEmail(email) {
+    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+}
